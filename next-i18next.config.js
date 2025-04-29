7cf0fb8d0729aab60643f2baa1f97f10b498c955
Gabriel Fonte <gabifonte@gmail.com>
2025-04-29 17:30:50 +0100

    fixed locale bug

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   i18n: {
     defaultLocale: 'en-US',
-    locales: ['en-US', 'pt', 'de-DE'],
+    locales: ['en-US', 'pt-PT', 'de-DE'],
     localeDetection: false,
     localePath: path.resolve('./public/locales'),
   },
